b9456a20a68a862d54b318bd4118382d82b8a176
Leila Pan <leila.pan@stonybrook.edu>
2022-10-12 18:03:20 -0400

    fixed modal UI

--- client/src/components/DeleteListModal.js
+++ client/src/components/DeleteListModal.js
@@ -32,7 +32,7 @@ function DeleteListModal(){
             id="delete-list-modal"
             data-animation="slideInOutLeft">
             <div className="modal-root modal-dialog" id='verify-delete-list-root'>
-                <div className="modal-north dialog-header ">
+                <div className="modal-north modal-header ">
                     Delete the {name} playlist?
                 </div>
                 <div className="modal-center">
@@ -40,9 +40,9 @@ function DeleteListModal(){
                         Are you sure you wish to permanently delete the {name} playlist?
                     </div>
                 </div>
-                <div className="modal-south">
-                    <input type="button" id="remove-song-confirm-button" className="modal-button" onClick={handleConfirmDeleteList} value='Confirm' />
-                    <input type="button" id="remove-song-cancel-button" className="modal-button" onClick={handleCancelDeleteList} value='Cancel' />
+                <div className="modal-south modal-footer" id="confirm-cancel-container">
+                    <input type="button" id="remove-song-confirm-button" className="modal-button modal-control" onClick={handleConfirmDeleteList} value='Confirm' />
+                    <input type="button" id="remove-song-cancel-button" className="modal-button modal-control" onClick={handleCancelDeleteList} value='Cancel' />
                 </div>
             </div>
         </div>
